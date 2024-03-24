c49a6a75401cf47fcb8bdee35ddfe3e63ab5926d
DEV_Topper <rehobothekene@gmail.com>
2024-03-25 00:53:52 +0100

    Dashboard-Update

--- myBitcion/src/App.tsx
+++ myBitcion/src/App.tsx
@@ -3,11 +3,11 @@ import { Router } from "./router/Router";
 import DashboardRouter from "./router/DashboardRouter";
 
 const App = () => {
-  return (
-    <div>
-      <RouterProvider router={Router} />
-    </div>
-  );
+	return (
+		<div>
+			<RouterProvider router={DashboardRouter} />
+		</div>
+	);
 };
 
 export default App;
